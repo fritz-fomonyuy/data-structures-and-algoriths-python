467afa7760325833104908d6ec13eedad34ff6e8
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-06-29 02:30:22 +0100

    solved more  arrayexercise.py

--- arrays/arrayExercises.py
+++ arrays/arrayExercises.py
@@ -21,11 +21,36 @@ print(my_array)
 
 #extend python array using extend() method
 
-my_array1=('i',[1,23,3,4,54])
-my_array.extend(my_array1)
-print(my_array)
+#my_array1=('i',[1,23,3,4,54])
+#my_array.extend(my_array1)
+#print(my_array)
 
 # add items from list to array using fromlist() method
 oddnum=[1,3,5,7]
 my_array.fromlist(oddnum)
 print(my_array)
+#remove last element from array using pop() method
+
+my_array.pop() 
+
+#fetch element through its index using index() method
+print(my_array.index(3))
+
+#reverse an array using revese() method
+
+print(my_array.reverse())
+#get array buffer information using buffer_info() method
+print(my_array.buffer_info())
+
+#check number of occurences  of elementsusing count()method
+print(my_array.count(2))
+
+#convert array to string using tostring() method
+strTemp = my_array.tostring()
+print(strTemp)
+ints = array('i')
+ints.fromstring(strTemp)
+print(ints)
+
+#slice elements in array
+print(my_array[1:4])
