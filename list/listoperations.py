99a1ee75cd054c005a4b6c13436ff659be7efce3
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-06-30 13:01:52 +0100

    created list folder and started with list and it.s operations

---
+++ list/listoperations.py
@@ -0,0 +1,14 @@
+names = ["fritz","mark","lafon","fabrice",""]
+ages = [1,2,2,4,5,6,12,3]
+print(ages)
+
+#access elements in list 
+print(ages[3])
+
+#traversing a list 
+for b in ages:
+    print(b)
+
+for i in range(len(names)):
+    names[i] = names[i]+ "+"
+    print(names[i])
