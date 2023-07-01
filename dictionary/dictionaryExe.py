8080a785a5e544c1af81b5f977847bc23d9e9245
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-07-01 04:02:26 +0100

    addedictionary traversing function in dictionary.py file

--- dictionary/dictionaryExe.py
+++ dictionary/dictionaryExe.py
@@ -9,3 +9,10 @@ print(word_enc["food"])
 #updating/add an value in dict
 word_enc["food"] = 3
 print(word_enc)
+
+#traversing a dictionary
+def traversing(dict):
+    for key in dict:
+      print(key, dict)
+
+traversing(word_enc)
