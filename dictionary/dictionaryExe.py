2fffe91f11749b6badc133a34647cc4252604ba6
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-07-01 03:57:36 +0100

    added accessing , update and adding a value in dictionary in dictionary.py file

---
+++ dictionary/dictionaryExe.py
@@ -0,0 +1,11 @@
+#create dictionary using dick() method
+myDict = dict()
+
+word_enc = {"food":"fritz","max":"dog"}
+
+#accesing a dict value we its keys
+print(word_enc["food"])
+
+#updating/add an value in dict
+word_enc["food"] = 3
+print(word_enc)
