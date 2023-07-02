2483e0c58b362de639bbb5c60c08009f43e89b8d
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-07-02 23:24:17 +0100

    added traversing of tuple method in tuples.py

--- tuple/tuples.py
+++ tuple/tuples.py
@@ -1,6 +1,14 @@
-new_tuple = (1,"2")
+new_tuple = (1,"2",2,2,2,3,4,23)
 new_tuple1 =  tuple()
 print(new_tuple)
 
 #accesing elements of tuple
 print(new_tuple[1])
+print(new_tuple[:4])
+
+#traversing a tuple
+for i in new_tuple:
+    print(i)
+for i in range(len(new_tuple)):
+    print(new_tuple[i])
+    
