0b080ea78733f12a2f2b5ae5e021c040c39f9cc3
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-07-17 21:52:50 +0100

    created insert function for double linked list

--- doubleLinkedList/DoubleLinkedList.py
+++ doubleLinkedList/DoubleLinkedList.py
@@ -21,8 +21,35 @@ class Double_LinkedList:
       self.head = node
       self.tail=node
       return "Double linked list created sucessfully"
-
+    
+    def insertNode(self,position,nodeValue):
+        if self.head is None:
+            return "node can't insert"
+        else:
+            node = Node(nodeValue)
+            if position == 0:
+                node.prev =None
+                node.next = self.head
+                self.head.prev = node
+                self.head = node
+            elif position == 1:
+                node.next = None
+                node.prev = self.tail
+                self.tail.next = node
+                self.tail = node
+            else:
+                tempnode = self.head 
+                index =0
+                while index < position - 1:
+                    tempnode = tempnode.next
+                    index +=1 
+                node.next = tempnode.next
+                node.prev = tempnode
+                node.next.prev= node
+                tempnode.next = node
+           
 Double_linked_list = Double_LinkedList()
 Double_linked_list.createDLL(12)
+Double_linked_list.insertNode(1,1)
 print([node.value for node in Double_linked_list])
 
