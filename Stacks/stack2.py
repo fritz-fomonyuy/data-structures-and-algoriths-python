dacb76c29ef80ff459b5c00b0ecb5a8fe18690aa
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-07-20 23:59:37 +0100

    created custom stacks with maxsize =12

--- Stacks/stack2.py
+++ Stacks/stack2.py
@@ -8,4 +8,31 @@ class Stack:
         values = [str(x) for x in self.list]
         return '\n'.join(values)
     
+    def isEmpty(self):
+        if self.list == None:
+            return True
+        else:
+            return False
+
+    def isFull(self):
+        if self.list == self.maxSize:
+            return True
+        else:
+            return False
+
+    def pop(self):
+        if self.isEmpty:
+            return "the stack is full"
+        else:
+            return self.list.pop()
     
+    def push(self,Value):
+        if self.isFull:
+            return "stack is full"
+        else:
+            self.list.append(Value)
+            return "the element has been sucessfuly appended"
+
+
+customStack = Stack(12)
+
