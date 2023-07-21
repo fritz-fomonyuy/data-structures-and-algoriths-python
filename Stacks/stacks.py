b128dca0e7e7bf1a1b0a5cf51809f81c84783a72
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-07-21 17:51:23 +0100

    added comments and doc string to stack.py file

--- Stacks/stacks.py
+++ Stacks/stacks.py
@@ -1,3 +1,6 @@
+''' --> stack implementation with list: unlimited size limit
+        class Stack initializes the list: str function converts the stack items
+        to strings and facilitates printing  '''
 class Stack:
     def __init__(self):
         self.list = []
@@ -7,27 +10,33 @@ class Stack:
         values = [str(x) for x in self.list]
         return '\n'.join(values)
 
+#checks if stack is empty
     def isEmpty(self):
         if self.list == []:
             return True
         else:
             return False
     
-    def push(self,value):
+# push an element to stack
+    def push(self,value): 
         self.list.append(value)
         return "element inserted sucessfully"
 
+#deletes the last element from the stack
     def pop(self):
         if self.isEmpty():
             return "stack is empty"
         else:
             self.list.pop()
-    
+
+#returns the last element without deleting   
     def peek(self):
         if self.isEmpty():
             return "True"
         else:
             return self.list[len(self.list)-1]
+
+#delete the stack
     def delete(self):
         self.list = None
         
