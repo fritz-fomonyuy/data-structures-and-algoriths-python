45d50f9c584ab0c1a0431ced7208d48a35591f9a
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-07-22 01:41:11 +0100

    created queue.py file in Queue directory added is empty method to check is queue is empty

---
+++ Queue/queue.py
@@ -0,0 +1,16 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
+    
+    def isEmpty(self):
+        if self.items == []:
+            return True
+        else:
+            return False
+        
+    
+    
