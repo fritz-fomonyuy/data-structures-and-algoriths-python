3a121fe323a7aa2904ef5fe097fd2ff38d193fce
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-08-04 03:08:38 +0100

    added search to search for Nodes

--- Tree_BinaryTree /binaryTree_list.py
+++ Tree_BinaryTree /binaryTree_list.py
@@ -10,4 +10,16 @@ class BinaryTree:
         self.customList[self.lastUsedIndex+1]=value
         self.lastUsedIndex += 1
         return "the value has been successfuly inserted"
-newBT = BinaryTree(3)  
+    
+    def searchNode(self,nodeValue):
+        for i in range(len(self.customList)):
+            if self.customList[i]== nodeValue:
+                return True
+        return False
+
+newBT = BinaryTree(10)  
+print(newBT.insertNode("Drinks"))
+print(newBT.insertNode("Hot"))
+print(newBT.insertNode("Cold"))
+print(newBT.insertNode("tea"))
+print(newBT.searchNode("Drinks"))
