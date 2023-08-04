91a020cb2bd94f0b185671c348f4e69995da019a
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-08-04 02:50:36 +0100

    created a function to insert Node

--- Tree_BinaryTree /binaryTree_list.py
+++ Tree_BinaryTree /binaryTree_list.py
@@ -3,5 +3,11 @@ class BinaryTree:
       self.customList = size * [None]
       self.lastUsedIndex = 0
       self.maxSize = size
-
+    
+    def insertNode(self,value):
+        if self.lastUsedIndex + 1 == self.maxSize:
+            return "the binarytree is full"
+        self.customList[self.lastUsedIndex+1]=value
+        self.lastUsedIndex += 1
+        return "the value has been successfuly inserted"
 newBT = BinaryTree(3)  
