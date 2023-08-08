10fa4edb84482ccb0a621a8d84011133208bc875
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-08-08 18:04:41 +0100

    tested if BTS preOrder traversal workd

--- binarySearchTree/binarySearch_Tree.py
+++ binarySearchTree/binarySearch_Tree.py
@@ -18,7 +18,26 @@ def insertNode(rootNode,NodeValue):
         else:
             insertNode(rootNode.rightChild, NodeValue)
     return "node has been sucessfuly inserted"
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
 newBTS = BSTNode(None)
 print(insertNode(newBTS, 100))
 print(insertNode(newBTS, 90))
 print(newBTS.data)
+preOrderTraversal(newBTS)
