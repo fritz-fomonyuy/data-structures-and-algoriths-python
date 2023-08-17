315cde66a79431ed755a33bd52332c4f75ad2ace
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-08-17 12:29:46 +0100

    created method to do levelordertraversal in Binary heap

--- binaryHeap/binary_heap.py
+++ binaryHeap/binary_heap.py
@@ -3,5 +3,20 @@ class Heap:
         self.customList = (size+1) * [None]
         self.heapSize = 0
         self.maxSize = size + 1
-
+def peekOfHeap(rootNode):
+    if not rootNode:
+        return 
+    else:
+        return rootNode.customList[1]
+def sizeOfHeap(rootNode):
+    if not rootNode:
+        return 
+    else:
+        return rootNode.heapSize
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        for i in range(1,rootNode.heapSize + 1):
+            print(rootNode.customList[i])
 newHeap = Heap(5)
