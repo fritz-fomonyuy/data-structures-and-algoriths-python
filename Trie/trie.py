d541d6b6db056a73ff2a557d90c53c32d0129f38
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-08-18 14:54:28 +0100

    added a method to search for strings in trie

--- Trie/trie.py
+++ Trie/trie.py
@@ -12,10 +12,22 @@ class Trie:
             ch = i
             node = current.children.get(ch)
             if node == None:
-                node = TrieNode
+                node = TrieNode()
                 current.children.update({ch:node})
             current = node
             current.endOfString = True
             print("inserted successfully")
-            
+    def searchString(self, word):
+            currentNode = self.root
+            for i in word:
+                node = currentNode.children.get(i)
+                if node == None:
+                    return False
+                currentNode = node
+            if currentNode.endOfString == True:
+                return True
+            else:
+                return False       
 newTrie = Trie()
+newTrie.insertString("assnal")
+print(newTrie.searchString("fer"))
