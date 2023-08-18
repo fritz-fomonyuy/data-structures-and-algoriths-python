075bdc3a77de9773a04b169cf3bfcf850e43e143
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-08-18 14:30:12 +0100

    added a method to nsert a node in Trie

---
+++ Trie/trie.py
@@ -0,0 +1,21 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfString = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insertString(self, word):
+        current = self.root
+        for i in word:
+            ch = i
+            node = current.children.get(ch)
+            if node == None:
+                node = TrieNode
+                current.children.update({ch:node})
+            current = node
+            current.endOfString = True
+            print("inserted successfully")
+            
+newTrie = Trie()
