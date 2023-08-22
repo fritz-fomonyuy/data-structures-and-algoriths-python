303647ca80c00fdec3ee5439c276be841e65de8a
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-08-22 04:00:12 +0100

    doccumented functions

--- arrays/2dArray.py
+++ arrays/2dArray.py
@@ -1,36 +1,107 @@
-#create a 2d array to store some random data
-
 import numpy as np
 
-twoDarray = np.array([[1,3,4,5,5],[3,6,7,5,7],[3,5,6,6,3]])
+twoDarray = np.array([[1, 3, 4, 5, 5], [3, 6, 7, 5, 7], [3, 5, 6, 6, 3]])
 print(twoDarray)
 
-twoDarray1 = np.insert(twoDarray,1,[[13,2,3]], axis=1)
+twoDarray1 = np.insert(twoDarray, 0, [[13, 2, 3, 4, 4]], axis=0)
 print(twoDarray1)
 
-#accesing 2d array element using a function
-def accesElement(array,rowIndex,columnIndex):
-    if rowIndex >= len(array) and columnIndex >= len(array[0]):
-        print("incorect index")
+def accessElement(array, rowIndex, columnIndex):
+    """
+    Accesses an element in a two-dimensional array based on the given row and column indices.
+
+    Parameters:
+        array (ndarray): The two-dimensional array.
+        rowIndex (int): The index of the row.
+        columnIndex (int): The index of the column.
+
+    Returns:
+        None
+
+    Prints the element at the specified row and column indices if they are within the array bounds.
+    If the indices are out of bounds, it prints an "Incorrect index" error message.
+
+    Example:
+        >>> accessElement(twoDarray1, 2, 3)
+        6
+    """
+    if rowIndex >= len(array) or columnIndex >= len(array[0]):
+        print("Incorrect index")
     else:
         print(array[rowIndex][columnIndex])
 
-accesElement(twoDarray1,2,5)
+accessElement(twoDarray1, 2, 3)
 
-#function to traverse 2d array
+
 def traverse2Darray(array):
+    """
+    Traverses a two-dimensional array and prints all its elements.
+
+    Parameters:
+        array (ndarray): The two-dimensional array.
+
+    Returns:
+        None
+
+    Prints all the elements of the array, row by row.
+
+    Example:
+        >>> traverse2Darray(twoDarray1)
+        13
+        2
+        3
+        4
+        4
+        1
+        3
+        4
+        5
+        5
+        3
+        6
+        7
+        5
+        7
+        3
+        5
+        6
+        6
+        3
+    """
     for i in range(len(array)):
         for j in range(len(array[0])):
-            print(array[1][j])
+            print(array[i][j])
 
-#searching element in 2d array
-def searchTDarray(array,value):
+traverse2Darray(twoDarray1)
+
+
+def search2Darray(array, value):
+    """
+    Searches for a value in a two-dimensional array and returns its index.
+
+    Parameters:
+        array (ndarray): The two-dimensional array to be searched.
+        value: The value to search for in the array.
+
+    Returns:
+        str: A string indicating the index of the first occurrence of the value in the array, if found.
+             If the value is not found, returns "Element is not found".
+
+    Searches for the specified value in the given two-dimensional array.
+    If the value is found, it returns a string indicating the index of its first occurrence.
+    If the value is not found, it returns a string indicating that the element is not found.
+
+    Example:
+        >>> search2Darray(twoDarray, 4)
+        'The value is at index 0 2'
+    """
     for i in range(len(array)):
         for j in range(len(array[0])):
-            if array[i][j]== value:
-              return "the value is at index "+str(i)+" "+str(j)
-    return "element is not found"
-print(searchTDarray(twoDarray,4))
+            if array[i][j] == value:
+                return "The value is at index " + str(i) + " " + str(j)
+    return "Element is not found"
 
-newTdarr = np.delete(twoDarray1,0,axis=0)
-print(twoDarray1)
+print(search2Darray(twoDarray, 4))
+
+newTdarr = np.delete(twoDarray1, 0, axis=0)
+print(newTdarr)
--- arrays/accessArrelEment.py
+++ arrays/accessArrelEment.py
@@ -2,21 +2,61 @@ from array import *
 
 arr_1 = array("d", [2.3,3.4,4.5,1.2])
 
-def accessArrElement(array,index):
-    if index > len(array):
-        print("element not exist")
+
+def accessArrElement(array, index):
+    """
+    Accesses an element in an array based on the given index.
+
+    Parameters:
+        array (list): The array or list containing the elements.
+        index (int): The index of the element to be accessed.
+
+    Returns:
+        None
+
+    Prints the element at the specified index if it exists in the array.
+    If the index is out of bounds (greater than or equal to the length of the array),
+    it prints a message indicating that the element does not exist.
+
+    Example:
+        >>> accessArrElement([10, 20, 30, 40], 2)
+        30
+        >>> accessArrElement(['a', 'b', 'c'], 5)
+        Element does not exist
+    """
+    if index >= len(array):
+        print("Element does not exist")
     else:
         print(array[index])
 
 accessArrElement(arr_1,8)
 
+def searchingElement(array, value):
+    """
+    Searches for an element in an array and returns its index.
 
-def seachingElement(array,value):
+    Parameters:
+        array (list): The array or list to be searched.
+        value: The element to search for in the array.
+
+    Returns:
+        int: The index of the first occurrence of the element in the array, if found.
+        str: "Element does not exist in the array" if the element is not found.
+
+    Searches for the specified value in the given array. If the element is found,
+    it returns the index of its first occurrence. If the element is not found,
+    it returns a string indicating that the element does not exist in the array.
+
+    Example:
+        >>> searchingElement([10, 20, 30, 40], 30)
+        2
+        >>> searchingElement(['a', 'b', 'c'], 'd')
+        Element does not exist in the array
+    """
     for i in array:
         if i == value:
             return array.index(value)
-    return "element does not exist in the array"
-
-seachingElement(arr_1,)
+    return "Element does not exist in the array"
+searchingElement(arr_1,2)
 
      
