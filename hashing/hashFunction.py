5549499c49893fe793daf6caffd5980fd1370e02
fomonyuy fritz <fritfomonyuy03@gmail.com>
2023-09-13 23:40:37 +0100

    i added some basic hash functions

---
+++ hashing/hashFunction.py
@@ -0,0 +1,14 @@
+def mod(interger, cellNum):
+    return interger % cellNum
+
+hashValue = mod(56 , 20)
+print(hashValue)
+
+def modASCII(string, cellnum):
+    total = 0
+    for i in string:
+        total += ord(i)
+    return total % cellnum
+
+hashValue2 = modASCII("fritz", 40)
+print(hashValue2)
